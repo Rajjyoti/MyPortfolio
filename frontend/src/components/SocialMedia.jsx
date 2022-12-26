c8953959463d523f7e92bfecaeca7c1173ae68a1
Rajjyoti <rajjyotidoley1@gmail.com>
2022-12-26 18:45:44 +0530

    update

--- frontend/src/components/SocialMedia.jsx
+++ frontend/src/components/SocialMedia.jsx
@@ -1,18 +1,24 @@
 import React from 'react'
-import {BsInstagram, BsLinkedin} from 'react-icons/bs'
+import {BsInstagram, BsLinkedin, BsTwitter} from 'react-icons/bs'
 import {AiOutlineMail} from 'react-icons/ai'
 
 const SocialMedia = () => {
   return (
     <div className='app__social'>
         <div>
+        <a href="https://www.linkedin.com/in/rajjyoti-doley-819a83179/" target='_blank'>
             <BsLinkedin />
+        </a>
         </div>
         <div>
+        <a href="https://www.instagram.com/doley_jr/" target='_blank'>
             <BsInstagram />
+            </a>
         </div>
         <div>
-            <AiOutlineMail />
+        <a href="https://twitter.com/rajjyotidoley" target='_blank'>
+            <BsTwitter />
+            </a>
         </div>
     </div>
   )
